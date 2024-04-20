20e29602400e75efb9dc5d74bf6123b82b7fa612
S10eb825 <fyen.work@gmail.com>
2024-04-20 16:33:31 +0900

    Rails構築

---
+++ README.md
@@ -0,0 +1,4 @@
+ # rails プロジェクト作成
+ rails new . --force --database=mysql   --skip-bundle --skip-turbolinks --skip-test --api
+docker-compose exec frontend /bin/bash
+docker-compose run --rm backend bash
---
+++ backend/start.sh
@@ -0,0 +1,19 @@
+# DBマイグレーション
+if [ $DO_MIGRATION = "true" ]; then
+  bundle exec rake db:migrate RAILS_ENV=$RAILS_ENV
+fi
+
+# whenever設定
+# bundle exec whenever -i rails-app --update-crontab --set "path=/app/&environment=$RAILS_ENV"
+
+# cronデーモン起動
+# service cron start
+
+# ログファイル作成
+
+
+# Unicornサーバー起動
+rm -f /var/run/unicorn.pid
+rm -f /app/tmp/pids/server.pid
+# bundle exec unicorn_rails --env $RAILS_ENV -c config/unicorn.rb
+rails server -b 0.0.0.0

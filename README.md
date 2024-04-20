887868be23b12a702f40c702180f63fd023a56a1
S10eb825 <fyen.work@gmail.com>
2024-04-20 16:45:22 +0900

    testデータ登録

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
  # rails プロジェクト作成
  rails new . --force --database=mysql   --skip-bundle --skip-turbolinks --skip-test --api
+ database.ymlの設定
  rails db:create
+ rails generate scaffold Test name:string age:integer
  rails db:migrate
+
+# テストデータ作成
+rails console
+Test.create(name: "John", age: 25)
+Test.create(name: "Alice", age: 30)
 
 docker-compose exec frontend /bin/bash
 docker-compose run --rm backend bash
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_073934) do
+  create_table "tests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
